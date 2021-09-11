fe03fba8487653b3d138869d65c7bdd2334037b9
0dinp0din <odin481@gmail.com>
2021-09-11 20:53:51 +0200

    Working on healthsystem

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,28 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UI;
 using UnityEngine;
-using TMPro;
 
 
 public class PlayerController : MonoBehaviour
 {
-    public TextMeshProUGUI KillCounter;
-
-    private int kills;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private HeartContainer hc;
+    public void DamagePlayer(int dmg)
     {
-        SetKillCounter();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void SetKillCounter()
-    {
-        KillCounter.text = "Kills:" + kills.ToString();
+        if (currHealth > 0)
+        {
+            currHealth -= dmg;
+                
+        }
     }
 }
--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerStats : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace Player {
+    [Serializable]
 
-    // Update is called once per frame
-    void Update()
-    {
+    public class PlayerStats {
+        public float movementSpeed = 5f;
+        public int currHealth = 1;
+        public int maxHealth = 1;
+        public int currStamina = 10;
+        public int maxStamina = 10;
+
         
+
     }
 }

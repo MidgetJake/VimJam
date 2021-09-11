499b16fd7feff00513cd3d9a22b795e906d902ad
0dinp0din <odin481@gmail.com>
2021-09-11 09:36:25 +0200

    Created UI element for killcounter

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class PlayerController : MonoBehaviour
 {
+    public TextMeshProUGUI KillCounter;
+
+    private int kills;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetKillCounter();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SetKillCounter()
+    {
+        KillCounter.text = "Kills:" + kills.ToString();
     }
 }

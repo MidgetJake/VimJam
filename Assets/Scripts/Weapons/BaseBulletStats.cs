41fad8008f15e75e5852272bc9a734f453c03c51
Jake Barter <midg3tjake@live.co.uk>
2021-09-12 20:32:23 +0100

    Started work on Upgrade
    We love nasty switch statements

--- Assets/Scripts/Weapons/BaseBulletStats.cs
+++ Assets/Scripts/Weapons/BaseBulletStats.cs
@@ -3,7 +3,7 @@ using System;
 namespace Weapons {
     [Serializable]
     public struct BaseBulletStats {
-        public int bulletDamage;
+        public float bulletDamage;
         public float bulletSpeed;
         public float bulletSize;
         public float lifetime;

4d2df62be429e392a0e529cc4e26c5dbeecb5548
Jake Barter <midg3tjake@live.co.uk>
2021-09-12 18:24:30 +0100

    Bullet/Shooting configuration

--- Assets/Scripts/Weapons/BaseWeaponStats.cs
+++ Assets/Scripts/Weapons/BaseWeaponStats.cs
@@ -4,6 +4,9 @@ namespace Weapons {
     [Serializable]
     public struct BaseWeaponStats {
         public int fireRate;
+        public int bulletCount;
+        public float[] bulletOffsets;
+        public float[] bulletFireDelay;
         public BaseBulletStats bulletStats;
     }
 }
